4b0f0fc408ddb549bb9caa4ae4d80eabfa3e4c35
Hakob Baghdasaryan <baghdasaryanhakob@gmail.com>
2020-11-18 01:04:37 +0400

    Git homework

---
+++ test.py
@@ -0,0 +1,11 @@
+#this is just a test file for git
+
+import math
+
+print("This program calculates the square root of a number")
+
+val = int(input("Enter any number: "))
+
+square_root = math.sqrt(val)
+
+print("The square root of", val, "is", square_root, ".")
